b16859c445f98d3d5110af561f929b5412b09069
slv177 <shambazov@gmail.com>
2015-05-22 23:15:23 +0300

    Initial Commit

---
+++ Sokoban/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sokoban
+//
+//  Created by Viacheslav Shambazov on 22/05/2015.
+//  Copyright (c) 2015 Viacheslav Shambazov. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
